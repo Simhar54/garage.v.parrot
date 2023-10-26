cb64d6b4f1363a6f9bce1fc32c39e4a139b3a894
Arnaud Simoncelli <a.simoncelli54@gmail.com>
2023-10-26 18:09:10 +0200

    Add Filter layout and fix some layout issue

--- public/assets/JS/carScript.js
+++ public/assets/JS/carScript.js
@@ -10,7 +10,6 @@ fetch('/api/cars')
     })
     .catch(error => {
         console.error('Erreur lors de la récupération des voitures:', error);
-        
         // Créer une alerte Bootstrap
         const alertDiv = document.createElement('div');
         alertDiv.className = 'alert alert-danger alert-dismissible fade show';
@@ -19,25 +18,34 @@ fetch('/api/cars')
             Une erreur est survenue lors de la récupération des voitures. Veuillez réessayer plus tard.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         `;
-    
-        // Ajouter l'alerte à un élément parent, par exemple un élément avec l'ID "alertContainer"
         const alertContainer = document.getElementById('alertContainer');
         alertContainer.appendChild(alertDiv);
     });
 
 
+    //escape html
+    function escapeHtml(unsafe) {
+        return unsafe.replace(/&/g, "&amp;")
+                     .replace(/</g, "&lt;")
+                     .replace(/>/g, "&gt;")
+                     .replace(/"/g, "&quot;")
+                     .replace(/'/g, "&#039;");
+    }
+    
+
+
     function addCardCarBootstrap($id, $brand, $model, $price, $year, $mileage, $image) {
         const cardCar = document.createElement('div');
         cardCar.id = $id;
-        cardCar.className = 'card col col-10 col-lg-3 mt-3 mx-3';
+        cardCar.className = 'card cardCar col col-10 col-lg-2 mt-3 mx-3';
         cardCar.innerHTML = `
             <div class="card-body">
-                <img src="../../images/cars/${$image}" class="card-img-top" alt="...">
-                <h5 class="card-title">${$brand}</h5>
-                <p class="card-text mt-2">${$model}</p>
-                <p class="card-text mt-2">Prix: ${$price}€</p>
-                <p class="card-text mt-2">Année: ${$year}</p>
-                <p class="card-text mt-2">Kilométrage: ${$mileage}km</p>
+                <img src="../../images/cars/${escapeHtml(String($image))}" class="card-img-top" alt="...">
+                <h5 class="card-title">${escapeHtml(String($brand))}</h5>
+                <p class="card-text mt-2">${escapeHtml(String($model))}</p>
+                <p class="card-text mt-2">Prix: ${escapeHtml(String($price))}€</p>
+                <p class="card-text mt-2">Année: ${String($year)}</p>
+                <p class="card-text mt-2">Kilométrage: ${String($mileage)}km</p>
                 <div class="text-center">
                     <a href="#" class="btn gp_button">Voir le detail</a>
                 </div>
